093217b61ab10c6bf3b6b1fabe0a5704a0a45515
Replit user <>
2022-08-14 21:02:27 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -68,7 +68,7 @@ typedef struct {
 
 Persona* crearPersona(char nombre[], char rut[], int edad) {
   struct Persona;
-  scanf("%[]", Persona.nombre);
+  scanf("%c", &nombre[30].Persona);
   return NULL;
 }
 
