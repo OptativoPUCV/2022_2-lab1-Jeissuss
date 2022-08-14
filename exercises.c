738b3dc5f51bf75470d06da42eafd5224eb95fc5
Replit user <>
2022-08-14 22:11:47 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -68,7 +68,7 @@ typedef struct {
 
 Persona* crearPersona(char nombre[], char rut[], int edad) {
   Persona* p =(Persona*)malloc(sizeof(Persona));
-  scanf("%s ",p->nombre);
+  scanf("%s ",&p.nombre);
   scanf("%s ",p->rut);
   scanf("%i",&p->edad);
   return NULL;
