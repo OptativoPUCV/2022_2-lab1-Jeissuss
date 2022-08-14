5c4433dd4ad7d2c41f9d54e076580d09720c602a
Replit user <>
2022-08-14 21:27:38 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -70,7 +70,7 @@ Persona* crearPersona(char nombre[], char rut[], int edad) {
   Persona* p =(Persona*)malloc(sizeof(Persona));
   scanf("%s %s %*i",p->nombre,p->rut,p->edad);
   
-  return *p;
+  return p;
 }
 
 /*
